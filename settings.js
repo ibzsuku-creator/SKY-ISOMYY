461071669b1f90b88cc49e50debf2f7cf7442993
ibzsuku-creator <ibzsuku@gmail.com>
2026-08-05 12:59:40 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    botName: 'SKY-ISOMYY',
+    developer: 'IB-SACKO',
+    developerNumber: '621963059',
+    ownerName: 'DIA 🥷',
+    ownerContact: '621705727',
+    system: 'CENTRAL-HEX',
+    version: '1.0.0',
+    menuImage: 'https://i.ibb.co/DH3dmGfq/IMG-20260801-WA0803.jpg'
+};
